8af80bb3863264356d044520838e854cbb27d6c3
shu1270-maker <syuuyataiko@gmail.com>
2026-03-20 14:13:15 +0900

    Add Discord bot with gomamayo detection

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+import os
+import re
+
+TOKEN = os.getenv('DISCORD_BOT_TOKEN_2')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+def is_gomamayo(text):
+    if re.search(r'(.)\1\1', text):
+        return False
+    if re.search(r'[をがはにも]', text):
+        return False
+    match = re.search(r'([\u3040-\u309F\u30A0-\u30FF])\1', text)
+    return True if match else False
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if is_gomamayo(message.content):
+        await message.channel.send("⁉️")
+
+if TOKEN:
+    client.run(TOKEN)
